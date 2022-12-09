966c55fa93b748ba95ce79a8b851f9949e1be469
coding-kiko <kikoaudi2001@gmail.com>
2022-12-09 00:47:18 -0300

    adjust table size

--- backend/internal/service/get_summary.go
+++ backend/internal/service/get_summary.go
@@ -75,7 +75,7 @@ var fixedTop = `<!DOCTYPE html>
 					</style>
 				</head>
 				<body>
-					<table class="styled-table">
+					<table class="styled-table" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;">
 						<thead>
 							<tr class="active-row">
 								<th>Month</th>
