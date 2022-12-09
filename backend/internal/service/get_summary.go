a5dd39896fef06a3f51795f46ad9a6ce1badbd6d
coding-kiko <kikoaudi2001@gmail.com>
2022-12-09 00:37:38 -0300

    file builder

--- backend/internal/service/get_summary.go
+++ backend/internal/service/get_summary.go
@@ -1,6 +1,11 @@
 package service
 
-import "control-pago-backend/internal/entity"
+import (
+	"bytes"
+	"control-pago-backend/internal/entity"
+	enc "encoding/base64"
+	"fmt"
+)
 
 func (s *service) GetSummary() (*entity.GetSummaryResponse, error) {
 	payments, err := s.Repo.GetPayments()
@@ -22,6 +27,74 @@ func (s *service) GetSummary() (*entity.GetSummaryResponse, error) {
 	return res, nil
 }
 
-func GenerateHtmlSummaryBase64([]entity.Payment) (string, error) {
-	return "", nil
+func GenerateHtmlSummaryBase64(payments []entity.Payment) (string, error) {
+	var file = bytes.NewBuffer(nil)
+
+	file.WriteString(fixedTop)
+	for _, payment := range payments {
+		row := fmt.Sprintf(dynamicRow, payment.Month, payment.Amount, payment.Company, payment.Receipt)
+		file.WriteString(row)
+	}
+	file.WriteString(fixedBot)
+
+	fileBase64 := enc.StdEncoding.EncodeToString(file.Bytes())
+
+	return fileBase64, nil
 }
+
+var fixedTop = `<!DOCTYPE html>
+				<html>
+				<head>
+					<style>
+						.styled-table {
+							border-collapse: collapse;
+							margin: 25px 0;
+							font-size: 0.9em;
+							font-family: sans-serif;
+							min-width: 400px;
+							box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+						}
+						.styled-table thead tr {
+							background-color: #353d3c;
+							color: #ffffff;
+							text-align: left;
+						}
+						.styled-table th,
+						.styled-table td {
+							padding: 12px 15px;
+						}
+						.styled-table tbody tr {
+							border-bottom: 1px solid #dddddd;
+						}
+						.styled-table tbody tr:nth-of-type(even) {
+							background-color: #f3f3f3;
+						}
+						.styled-table tbody tr:last-of-type {
+							border-bottom: 2px solid #353d3c;
+						}
+					</style>
+				</head>
+				<body>
+					<table class="styled-table">
+						<thead>
+							<tr class="active-row">
+								<th>Month</th>
+								<th>Amount</th>
+								<th>Company</th>
+								<th>Receipt</th>
+							</tr>
+						</thead>
+						<tbody>`
+
+var fixedBot = `		</tbody>
+					</table>
+				</body>
+
+				</html>`
+
+var dynamicRow = `<tr>
+					<td>%s</td>
+					<td>%d</td>
+					<td>%d</td>
+					<td><a href="%s">view</td>
+				   </tr>`
